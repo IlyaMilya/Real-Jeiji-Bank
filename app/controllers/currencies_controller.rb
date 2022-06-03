5c2ea6fba304b09f5622fe6f40eddb4c1a14b2d4
Ilya <milyavskyfamily@gmail.com>
2022-06-03 10:43:22 -0400

    added crud

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -4,6 +4,35 @@ class CurrenciesController < ApplicationController
         render json: currencies 
     end
 
+ def show 
+    currency = Currency.find_by(id: params[:id])
+    if currency
+    render json: currency, status:200 
+    else 
+    render json: {error: "Currency not found"}, status:404 
+ end
+end
+
+ def update 
+    currency = Currency.find_by(id: params[:id])
+    if currency 
+    currency.update
+    render json: currency, status:200 
+    else 
+    render json: {error: "update unsuccessful" }
+ end
+end
+
+    def create 
+        currencycreate = Currency.create(params.permit)
+        if currencycreate.valid?
+            render json: currencycreate, status: 201
+        else 
+        render json: {error: "failed to create"}, status: 422
+    end 
+end
+
+
     def destroy 
         deletecur = Currency.find_by(id:params[:id])
         if deletecur 
@@ -13,4 +42,11 @@ class CurrenciesController < ApplicationController
             render json: {"Delete unsuccessful"}, status:406
     end
 
+
+    private 
+
+    def params_permit 
+        params.permit(:name, :price, :image)
+    end 
 end
+
